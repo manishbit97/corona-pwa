81c5595c42fa7c0bde25a00377d9a0084d965166
Manish Kumar <manish.kumar@gaadi.com>
2022-02-01 03:43:37 +0530

    second commit package cahnges

---
+++ backend/services/mohfw/apimap.js
@@ -0,0 +1,186 @@
+var apimap = {
+    10: {
+        "state_name": "Bihar",
+        "state_code": "10",
+        "text_code": "BR"
+    },
+    "11": {
+        "state_name": "Sikkim",
+        "state_code": "11",
+        "text_code": "SK"
+    },
+    "12": {
+        "state_name": "Arunachal Pradesh",
+        "state_code": "12",
+        "text_code": "AR"
+    },
+    "13": {
+        "state_name": "Nagaland",
+        "state_code": "13",
+        "text_code": "NL"
+    },
+    "14": {
+        "state_name": "Manipur",
+        "state_code": "14",
+        "text_code": "MN"
+    },
+    "15": {
+        "state_name": "Mizoram",
+        "state_code": "15",
+        "text_code": "MZ"
+    },
+    "16": {
+        "state_name": "Tripura",
+        "state_code": "16",
+        "text_code": "TR"
+    },
+    "17": {
+        "state_name": "Meghalaya",
+        "state_code": "17",
+        "text_code": "ML"
+    },
+    "18": {
+        "state_name": "Assam",
+        "state_code": "18",
+        "text_code": "AS"
+    },
+    "19": {
+        "state_name": "West Bengal",
+        "state_code": "19",
+        "text_code": "WB"
+    },
+    "20": {
+        "state_name": "Jharkhand",
+        "state_code": "20",
+        "text_code": "JH"
+    },
+    "21": {
+        "state_name": "Odisha",
+        "state_code": "21",
+        "text_code": "OD"
+    },
+    "22": {
+        "state_name": "Chhattisgarh",
+        "state_code": "22",
+        "text_code": "CT"
+    },
+    "23": {
+        "state_name": "Madhya Pradesh",
+        "state_code": "23",
+        "text_code": "MP"
+    },
+    "24": {
+        "state_name": "Gujarat",
+        "state_code": "24",
+        "text_code": "GJ"
+
+    },
+    "26": {
+        "state_name": "Dadra and Nagar Haveli and Daman and Diu",
+        "state_code": "26",
+        "text_code": "DD"
+    },
+    "27": {
+        "state_name": "Maharashtra",
+        "state_code": "27",
+        "text_code": "MH"
+    },
+    "28": {
+        "state_name": "Andhra Pradesh",
+        "state_code": "28",
+        "text_code": "AP"
+    },
+    "29": {
+        "state_name": "Karnataka",
+        "state_code": "29",
+        "text_code": "KA"
+    },
+    "30": {
+        "state_name": "Goa",
+        "state_code": "30",
+        "text_code": "GA"
+    },
+    "31": {
+        "state_name": "Lakshadweep",
+        "state_code": "31",
+        "text_code": "LD"
+    },
+    "32": {
+        "state_name": "Kerala***",
+        "state_code": "32",
+        "text_code": "KL"
+    },
+    "33": {
+        "state_name": "Tamil Nadu",
+        "state_code": "33",
+        "text_code": "TN"
+    },
+    "34": {
+        "state_name": "Puducherry",
+        "state_code": "34",
+        "text_code": "PY"
+    },
+    "35": {
+        "state_name": "Andaman and Nicobar Islands",
+        "state_code": "35",
+        "text_code": "AN"
+    },
+    "36": {
+        "state_name": "Telangana",
+        "state_code": "36",
+        "text_code": "TS"
+    },
+    "37": {
+        "state_name": "Ladakh",
+        "state_code": "37",
+        "text_code": "LD"
+    },
+    "01": {
+        "state_name": "Jammu and Kashmir",
+        "state_code": "01",
+        "text_code": "JK"
+    },
+    "02": {
+        "state_name": "Himachal Pradesh",
+        "state_code": "02",
+        "text_code": "HP"
+    },
+    "03": {
+        "state_name": "Punjab",
+        "state_code": "03",
+        "text_code": "PB"
+    },
+    "04": {
+        "state_name": "Chandigarh",
+        "state_code": "04",
+        "text_code": "CH"
+    },
+    "05": {
+        "state_name": "Uttarakhand",
+        "state_code": "05",
+        "text_code": "UK"
+    },
+    "06": {
+        "state_name": "Haryana",
+        "state_code": "06",
+        "text_code": "HR"
+    },
+    "07": {
+        "state_name": "Delhi",
+        "state_code": "07",
+        "text_code": "DL"
+    },
+    "08": {
+        "state_name": "Rajasthan",
+        "state_code": "08",
+        "text_code": "RJ"
+    },
+    "09": {
+        "state_name": "Uttar Pradesh",
+        "state_code": "09",
+        "text_code": "UP"
+    }
+}
+
+
+module.exports = apimap
--- backend/services/mohfw/scrapeService.js
+++ backend/services/mohfw/scrapeService.js
@@ -3,7 +3,7 @@ const axios = require('axios');
 const cacheHelper = require('../../services/_helper/cacheHelper');
 const cheerio = require('cheerio');
 const rp = require('request-promise');
-
+const apimap = require('./apimap');
 module.exports = {
     scrapeMohfwApi,
     getScrapData
@@ -21,6 +21,10 @@ let formatApiResponse = (apidata) => {
             delete element['cured'];
             delete element['positive'];
             delete element['death'];
+            let state_code = 10;
+            console.log(state_code, apimap[state_code + '']);
+            element['text_code'] = apimap[element['state_code']] ? apimap[element.state_code]["text_code"] : '';
+            element['id'] = element['text_code'];
         })
     }
     return apidata;
@@ -47,7 +51,7 @@ async function getScrapData() {
                 const $ = cheerio.load(html);
                 const totalVaccin = $('#site-dashboard > div > div > div:nth-child(2) > div.col-xs-8.site-stats-count.sitetotal > div > span.coviddata').text();
                 const time = $('#site-dashboard > div > div > div:nth-child(1) > div:nth-child(1) > h5 > span').text().split('IST')[0].split('on :')[1].trim();
-                const delta_vaccin = $("#site-dashboard > div > div > div:nth-child(2) > div.col-xs-8.site-stats-count.sitetotal > div > span.coviddataval").text().replace(/[():]/g,'').trim();
+                const delta_vaccin = $("#site-dashboard > div > div > div:nth-child(2) > div.col-xs-8.site-stats-count.sitetotal > div > span.coviddataval").text().replace(/[():]/g, '').trim();
                 console.log(delta_vaccin);
                 const vaccin_timestamp = $('#site-dashboard > div > div > div:nth-child(2) > div.col-xs-2 > h6 > span').text().split("on :")[1].trim()
                 const delta_test = $('#main-content > header > div > div > div > div:nth-child(1) > div > marquee > span').text().split('day ')[1].trim()
